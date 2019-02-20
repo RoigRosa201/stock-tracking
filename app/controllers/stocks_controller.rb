31bc477edd28f72ab6938e23654c6f9d1169ff50
roig <roigrosa88@gmail.com>
2019-02-20 13:22:14 -0500

    modified css, controller, html and portfolio. Still need to fix ticker search and exception

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,20 @@
 class StocksController < ApplicationController
     
     def search
+    if params[:stock].present?
         Stock.new_from_lookup(params[:stock])
+        if @stock
         render 'users/my_portfolio'
+        else
+    
+        flash[:danger] = "you have entered an incorrect stock symbol"
+        redirect_to my_portfolio_path
+        end
+
+    else
+        flash[:danger] = "you have entered and improper searching string"
+        redirect_to my_portfolio_path
+    end
     end
 
 end
